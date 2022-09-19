cf4b974f51a86ca5a73ea4c1e9833877c2a920b6
anuj <chauhan.anuj1999@gmail.com>
2022-09-19 14:57:57 +0530

    dev environment setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Home from './Home.js'
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1> lets build airbnb clone</h1>
+          <Home/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
